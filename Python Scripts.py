93b43ee7cf5edaf2d7200b8d59e0ac04d2f4797c
Rauf <axm200065@utdallas.edu>
2022-06-24 02:16:19 -0500

    Trying out .py formats

---
+++ Python Scripts.py
@@ -0,0 +1,7 @@
+# I generally right in a Jupyter notebook, but I have been working around to using PyCharm.
+# With PyCharm we can import projects to a desktop and upload projects to github aswell.
+# The only thing with PyCharm is that it is paid except for a student version so we have a limited time to use it.
+# While google colab is a better alterative to saving specific notebooks to a project.
+
+import numpy as np
+
